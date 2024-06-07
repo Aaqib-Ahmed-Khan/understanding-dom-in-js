d9dca035c21b54405bff0e745b4f1b1353b4ef86
Aaqib-Ahmed-Khan <155422396+Aaqib-Ahmed-Khan@users.noreply.github.com>
2024-06-07 19:59:23 +0500

    settimeout

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // let a=6;
 // console.log(a*a);
+// let x= document.querySelector("h1");
+// console.log(x);
+// x.style.color="red";
+// x.style.fontFamily="gill sans";
 let x= document.querySelector("h1");
-console.log(x);
-x.style.color="red";
-x.style.fontFamily="gill sans";
+setTimeout(function(){
+x.innerHTML="ahmed";
+x.style.color="green";
+},3000
+)
