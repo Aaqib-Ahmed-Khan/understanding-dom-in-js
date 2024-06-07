e86614f7ae0d794e74b9b5a47cde9c48016e238f
Aaqib-Ahmed-Khan <155422396+Aaqib-Ahmed-Khan@users.noreply.github.com>
2024-06-07 21:04:29 +0500

    addeventlistener

--- script.js
+++ script.js
@@ -4,9 +4,16 @@
 // console.log(x);
 // x.style.color="red";
 // x.style.fontFamily="gill sans";
-let x= document.querySelector("h1");
-setTimeout(function(){
+let ele1= document.getElementById("h1");
+ele1.addEventListener("click",function(){
 x.innerHTML="ahmed";
 x.style.color="green";
 },3000
 )
+let ele2=document.getElementById("h2");
+ele1.addEventListener("mousemove",function()  {
+    y.innerHTML="im fine";
+    y.style.color="blue";
+
+    
+},4000 );
