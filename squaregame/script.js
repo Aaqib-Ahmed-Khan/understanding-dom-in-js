f3551805f2c31d1d90a5c0f297d6c7eb27995957
Aaqib-Ahmed-Khan <155422396+Aaqib-Ahmed-Khan@users.noreply.github.com>
2024-06-08 14:50:05 +0500

    color working toggle

--- squaregame/script.js
+++ squaregame/script.js
@@ -1,8 +1,46 @@
-var s1=document.getElementById("sq1");
-s1.addEventListener("mouseenter",function(){
-    s1.style.backgroundColor="red";
-})
-var s1=document.getElementById("sq1");
-s1.addEventListener("mouseleave",function(){
-    s1.style.backgroundColor="white";
-})
+var s1 = document.getElementById("sq1");
+s1.addEventListener("mouseenter", function() {
+    var r = Math.floor(Math.random() * 100000);
+    s1.innerHTML = `<h1>${r}</h1>`;
+});
+s1.addEventListener("mouseleave", function() {
+    s1.innerHTML = "<h1>1</h1>";
+});
+
+var s2 = document.getElementById("sq2");
+var clr = "red";
+s2.addEventListener("mouseenter", function() {
+    if (clr == "green") {
+        s2.style.backgroundColor = "yellow";
+        clr = "yellow";
+    } else {
+        s2.style.backgroundColor = "green";
+        clr = "green";
+    }
+});
+s2.addEventListener("mouseleave", function() {
+    s2.style.backgroundColor = "white";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var s3 = document.getElementById("sq3");
+s3.addEventListener("mouseenter", function() {
+    var r = Math.floor(Math.random() * 100000);
+    s3.innerHTML = `<h1>${r}</h1>`;
+});
+s3.addEventListener("mouseleave", function() {
+    s3.innerHTML = "<h1>1</h1>";
+});
+
