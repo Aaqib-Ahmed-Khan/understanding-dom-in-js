1d6cf378030b05e1fa9f70785e355cc6c4a7050f
Aaqib-Ahmed-Khan <155422396+Aaqib-Ahmed-Khan@users.noreply.github.com>
2024-06-08 14:24:28 +0500

    working box 1

---
+++ squaregame/script.js
@@ -0,0 +1,8 @@
+var s1=document.getElementById("sq1");
+s1.addEventListener("mouseenter",function(){
+    s1.style.backgroundColor="red";
+})
+var s1=document.getElementById("sq1");
+s1.addEventListener("mouseleave",function(){
+    s1.style.backgroundColor="white";
+})
